fff24d76cdb2a1039f3ba3d17ac174ed075c8ed4
dennisstilllearn <muktidennis07@gmail.com>
2024-11-17 14:24:06 +0800

    styling home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,37 @@ export default function Page() {
   }
   return (
     <>
-      <h1>Home</h1>
-
-      <ul>
-        <Link href={"/form"}>Form Link</Link>
-      </ul>
-      <ul>
-        <Link href={"/pokemon"}>Pokemon List</Link>
-      </ul>
-      <hr className={"my-6"} />
-      <form action={logout}>
-        <button type={"submit"}>Logout</button>
-      </form>
+      <div className="container mx-auto p-6 bg-white shadow-lg rounded-lg">
+        <div className="flex justify-between items-center p-6 bg-white shadow-md rounded-lg">
+          <h1 className="text-3xl font-bold text-black text-left">Home</h1>
+          <form action={logout} className="text-right">
+            <button
+              type="submit"
+              className="px-6 py-2 bg-gray-500 text-white font-semibold rounded-full hover:bg-gray-600"
+            >
+              Logout
+            </button>
+          </form>
+        </div>
+        <ul className="flex gap-8 mb-6 ml-5 mt-5">
+          <li>
+            <Link
+              href={"/form"}
+              className="text-lg font-medium text-indigo-600 hover:text-indigo-800"
+            >
+              Form Link
+            </Link>
+          </li>
+          <li>
+            <Link
+              href={"/pokemon"}
+              className="text-lg font-medium text-indigo-600 hover:text-indigo-800"
+            >
+              Pokemon List
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
