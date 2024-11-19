79c5802d6909c97a4c323866fb476ed74b0d3bbf
dennisstilllearn <muktidennis07@gmail.com>
2024-11-19 08:25:52 +0800

    Tasks 1 point 5 optimize re-render

--- src/components/form/fields/TextField.tsx
+++ src/components/form/fields/TextField.tsx
@@ -1,29 +1,40 @@
+import React, { useEffect } from "react";
 import { TextFieldProps } from "@/components/form/types";
 import { Input } from "@/components/ui/input";
-import React, { useEffect } from "react";
 
-const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ field, value, path, updateModelValue, ...props }, ref) => {
-    useEffect(() => {
-      console.log(`TextField ${path} mounted`);
-      return () => {
-        console.log(`TextField ${path} unmounted`);
-      };
-    }, []);
+const TextField = React.memo(
+  React.forwardRef<HTMLInputElement, TextFieldProps>(
+    ({ field, value, path, updateModelValue, ...props }, ref) => {
+      useEffect(() => {
+        console.log(`TextField ${path} mounted`);
+        return () => {
+          console.log(`TextField ${path} unmounted`);
+        };
+      }, []);
 
-    useEffect(() => {
-      console.log(`TextField ${path} rerendered`);
-    });
+      useEffect(() => {
+        console.log(`TextField ${path} rerendered`);
+      });
 
+      return (
+        <Input
+          ref={ref}
+          name={field.name}
+          id={path}
+          // @ts-ignore
+          value={value}
+          onChange={(e) => updateModelValue(path, field, e.target.value)}
+          {...props}
+        />
+      );
+    }
+  ),
+  (prevProps, nextProps) => {
+    // saya membuat comparison logic untuk menemukan perbedaan antara props sebelumnya dan props saat ini
     return (
-      <Input
-        ref={ref}
-        name={field.name}
-        id={path}
-        // @ts-ignore
-        value={value}
-        onChange={(e) => updateModelValue(path, field, e.target.value)}
-      />
+      prevProps.value === nextProps.value &&
+      prevProps.field.name === nextProps.field.name &&
+      prevProps.path === nextProps.path
     );
   }
 );
